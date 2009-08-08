b7c3095a61953fcb2c33cdef78f7ce0e1a8c1f81
Marius Nuennerich <marius@nuenneri.ch>
2009-08-08 09:58:37 +0200

    more tests

--- test/zsnap_test.rb
+++ test/zsnap_test.rb
@@ -4,7 +4,6 @@ require 'lib/zsnap'
 include Zsnap
 
 class RuleTest < Test::Unit::TestCase
-
   def test_rule
     r = Rule.new
     2.times do
@@ -27,11 +26,9 @@ class RuleTest < Test::Unit::TestCase
      assert !r.condition_met?(i)
     end
   end
-
 end
 
 class PreservationRuleTest < Test::Unit::TestCase
-
   def test_create
     p = PreservationRule.new
     assert p.applies?(239, 0)
@@ -45,5 +42,32 @@ class PreservationRuleTest < Test::Unit::TestCase
     assert p.condition_met_for_snapshot? 17, s
 
   end
+end
 
+class SnapshotInfoTest < Test::Unit::TestCase
+  def test_create
+    s = SnapshotInfo.new 'foo-2009-08-05_21.42', 'bar', 'foo-'
+    assert_equal 20825022, s.creation_time_minutes
+    assert_equal 'bar@foo-2009-08-05_21.42', s.canonical_name
+  end
+
+  def test_raises_error
+    s = SnapshotInfo.new 'foo-2009-08-05_21.42', nil, 'foo-'
+    assert_raise(RuntimeError) { s.canonical_name }
+  end
+end
+
+class FSInfoTest < Test::Unit::TestCase
+  def test_create
+    rules = {
+              'creation_rule' => {'at_multiple' => 60, 'offset' => 0 },
+              'preservation_rules' => [
+                { 'for_minutes' => 240, 'at_multiple' => 0, 'offset' => 0 }
+              ]
+            }
+    f = FSInfo.new 'tank/foo', rules
+    assert f.create?(60)
+# Mock here
+#    assert_equal 'ff', f.send(:get_mount_point, 'tank/foo')
+  end
 end
